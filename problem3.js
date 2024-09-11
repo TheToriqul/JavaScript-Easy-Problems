8ed52526e3f708f177e95fa9f71355aa2548df83
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-11 19:59:03 +0800

    problem 3

--- problem3.js
+++ problem3.js
@@ -5,6 +5,21 @@ function checkDigitsInName(name) {
   return /\d/.test(name);
 }
 
+// Another easiest implementation
+function checkDigitsInName(name) {
+  if (typeof name !== "string") {
+    return "Invalid Input";
+  }
+
+  for (let i = 0; i < name.length; i++) {
+    if (name[i] >= '0' && name[i] <= '9') {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 // Test cases
 console.log("\nProblem 3: Check Digits in Name");
 console.log(checkDigitsInName("Raj123"));
