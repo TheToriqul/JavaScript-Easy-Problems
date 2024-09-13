cb86084e18443a4c6a0a9809c37f5d505af836dc
Md Toriqul Islam <137867238+TheToriqul@users.noreply.github.com>
2024-09-13 21:27:43 +0800

    Feat: Added Test case and remove practice files.

--- waitingTime.js
+++ waitingTime.js
@@ -1,6 +1,10 @@
 // Easiest way to calculate for me actually
-function  waitingTime(waitingTimes  , serialNumber) {
-  if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number' || serialNumber <= waitingTimes.length) {
+function waitingTime(waitingTimes, serialNumber) {
+  if (
+    !Array.isArray(waitingTimes) ||
+    typeof serialNumber !== "number" ||
+    serialNumber <= waitingTimes.length
+  ) {
     return "Invalid Input";
   }
   let totalTime = 0;
@@ -8,20 +12,39 @@ function  waitingTime(waitingTimes  , serialNumber) {
     totalTime += waitingTimes[i];
   }
   let averageWaitingTime = Math.round(totalTime / waitingTimes.length);
-  let peopleAhead = serialNumber - waitingTimes.length -1;
+  let peopleAhead = serialNumber - waitingTimes.length - 1;
   let totalWaitingTime = averageWaitingTime * peopleAhead;
   return totalWaitingTime;
 }
 
 // Another way to calculate
 function waitingTime(waitingTimes, serialNumber) {
-  if (!Array.isArray(waitingTimes) || typeof serialNumber !== 'number' || serialNumber <= waitingTimes.length) {
+  if (
+    !Array.isArray(waitingTimes) ||
+    typeof serialNumber !== "number" ||
+    serialNumber <= waitingTimes.length
+  ) {
     return "Invalid Input";
   }
 
-  const averageTime = Math.round(waitingTimes.reduce((sum, time) => sum + time, 0) / waitingTimes.length);
+  const averageTime = Math.round(
+    waitingTimes.reduce((sum, time) => sum + time, 0) / waitingTimes.length
+  );
   const remainingPeople = serialNumber - waitingTimes.length - 1;
-  
+
   return averageTime * remainingPeople;
 }
 
+// Test cases
+console.log("\nProblem 5: Calculate Waiting Time");
+console.log(waitingTime([3, 5, 7, 11, 6], 10));
+console.log(waitingTime([13, 2], 6));
+console.log(waitingTime([13, 2, 6, 7, 10], 6));
+console.log(waitingTime([6], 4));
+console.log(waitingTime(7, 10));
+console.log(waitingTime("[6,2]", 9));
+console.log(waitingTime([7, 8, 3, 4, 5], "9"));
+
+// Additional test cases
+console.log(waitingTime([5, 10, 15], 7));
+console.log(waitingTime([1, 2, 3, 4, 5], 10));
